f287d52abdd7d7f8a49c0484650ae18ff6118b03
FFY00 <filipe.lains@gmail.com>
2019-02-15 23:42:14 +0000

    source: add blink example

---
+++ src/blink.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define LED 13
+
+void setup()
+{
+    pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED, HIGH);
+    delay(200);
+    digitalWrite(LED, LOW);
+    delay(200);
+}
